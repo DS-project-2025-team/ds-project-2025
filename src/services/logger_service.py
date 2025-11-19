8978a2a4d21ba256b7508fc6b067f575daa152ee
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-11-19 20:03:49 +0200

    refactor: Add types

--- src/services/logger_service.py
+++ src/services/logger_service.py
@@ -3,7 +3,7 @@ from logging import Logger, getLogger
 
 class LoggerService:
     def __init__(self, logger: Logger) -> None:
-        self.__logger = logger
+        self.__logger: Logger = logger
 
     def info(self, message: str) -> None:
         self.__logger.info(message)
