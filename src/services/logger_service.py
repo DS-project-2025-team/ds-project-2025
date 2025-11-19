96c6ee348e96daaa05287af3cdf526a5f12d74ce
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-11-19 19:46:55 +0200

    feat: Wrap logging into LoggerService

---
+++ src/services/logger_service.py
@@ -0,0 +1,21 @@
+from logging import Logger, getLogger
+
+
+class LoggerService:
+    def __init__(self, logger: Logger) -> None:
+        self.__logger = logger
+
+    def info(self, message: str) -> None:
+        self.__logger.info(message)
+
+    def error(self, message: str) -> None:
+        self.__logger.error(message)
+
+    def debug(self, message: str) -> None:
+        self.__logger.debug(message)
+
+    def warning(self, message: str) -> None:
+        self.__logger.warning(message)
+
+
+logger = LoggerService(getLogger(__name__))
