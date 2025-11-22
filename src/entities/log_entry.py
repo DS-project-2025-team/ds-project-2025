8304a88738adaea7420dd0d69e102c72ec3cd88a
phuvio <petteri.huvio@gmail.com>
2025-11-22 17:43:24 +0200

    feat: Add LogEntry

--- src/entities/log_entry.py
+++ src/entities/log_entry.py
@@ -1,3 +1,4 @@
 class LogEntry:
-    def operate(self) -> None:
-        pass
+    def __init__(self, term: int, command: dict) -> None:
+        self.term = term        # Raft term number
+        self.command = command  # Command to be applied to the state machine
--- src/entities/raft_log.py
+++
@@ -1,6 +0,0 @@
-class RaftLog:
-    def commit(self) -> None:
-        pass
-
-    def revert(self) -> None:
-        pass
