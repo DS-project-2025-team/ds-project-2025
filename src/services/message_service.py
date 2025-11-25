bbca6239c188b594de33dd75b39020c6d637849e
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-11-25 15:33:14 +0200

    feat: Add MessageService.send()
    Co-authored-by: phuvio <petteri.huvio@gmail.com>

--- src/services/message_service.py
+++ src/services/message_service.py
@@ -5,5 +5,8 @@ class MessageService:
     def __init__(self, producer: AIOKafkaProducer) -> None:
         self.__producer: AIOKafkaProducer = producer
 
+    async def send(self, topic: str, message: bytes) -> None:
+        await self.__producer.send_and_wait(topic, message)
+
     async def start(self) -> None:
         await self.__producer.start()
