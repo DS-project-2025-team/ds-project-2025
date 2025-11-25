b50c589c92993b137ba1f2091ebcba9887b03ec3
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-11-25 15:32:05 +0200

    feat: Initialize MessageService
    Co-authored-by: phuvio <petteri.huvio@gmail.com>

--- src/services/message_service.py
+++ src/services/message_service.py
@@ -1,2 +1,9 @@
+from aiokafka import AIOKafkaProducer
+
+
 class MessageService:
-    pass
+    def __init__(self, producer: AIOKafkaProducer) -> None:
+        self.__producer: AIOKafkaProducer = producer
+
+    async def start(self) -> None:
+        await self.__producer.start()
