08b8bdf27e9906eb437afe274c4e2ea9c580e3af
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-11-27 19:40:16 +0200

    fix: Change main to async

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import asyncio
+
 from entities.node import Node
 from services.message_service import MessageService
 
-if __name__ == "__main__":
+
+async def main() -> None:
     message_service = MessageService("localhost", 9092)
     node = Node(message_service=message_service)
 
     node.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
