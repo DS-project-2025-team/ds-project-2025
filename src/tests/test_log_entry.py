03bce9003db099b903ff237db38791094047fc01
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-11-28 18:55:34 +0200

    refactor(test): Remove useless test

--- src/tests/test_log_entry.py
+++
@@ -1,9 +0,0 @@
-from entities.log_entry import LogEntry
-
-
-def test_log_entry_init():
-    cmd = {"type": "NODE_JOIN", "node_id": "node1"}
-    entry = LogEntry(term=1, command=cmd)
-
-    assert entry.term == 1
-    assert entry.command == cmd
