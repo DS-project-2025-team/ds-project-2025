694e1a6bf2775e0be398b855d61ebc6563394d2a
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-11-30 18:55:59 +0200

    feat: Extract MessageConsumer

---
+++ src/network/message_consumer.py
@@ -0,0 +1,64 @@
+import json
+from contextlib import AbstractAsyncContextManager
+from types import TracebackType
+from typing import Self
+
+from aiokafka import AIOKafkaConsumer
+
+from logger_service import logger
+
+
+def deserializer(serialized: str) -> dict:
+    return json.loads(serialized)
+
+
+class MessageConsumer(AbstractAsyncContextManager):
+    def __init__(self, server: str, port: int = 9092, groupid: str = "foo") -> None:
+        self.__consumer: AIOKafkaConsumer = AIOKafkaConsumer(
+            group_id=groupid,
+            bootstrap_servers=f"{server}:{port}",
+            value_deserializer=deserializer,
+            auto_offset_reset="earliest",
+        )
+
+    async def commit(self) -> None:
+        await self.__consumer.commit()
+
+    async def receive(self) -> dict:
+        messages = await self.__consumer.getmany()
+        await self.__consumer.commit()
+        for tp, msgs in messages.items():
+            for msg in msgs:
+                logger.info(
+                    "Received message to %s: %r (partition=%s offset=%s)",
+                    msg.topic,
+                    msg.value,
+                    msg.partition,
+                    msg.offset,
+                )
+
+        return messages
+        # return await self.__consumer.getmany()
+
+    def subscribe(self, *topics: str) -> None:
+        """
+        Override currently subscribed topics.
+        """
+
+        self.__consumer.subscribe(topics)
+
+    async def __aenter__(self) -> Self:
+        await self.__consumer.start()
+
+        return self
+
+    async def stop(self) -> None:
+        await self.__consumer.stop()
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        await self.__consumer.stop()
