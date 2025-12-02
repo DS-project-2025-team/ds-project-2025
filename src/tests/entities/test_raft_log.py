0f86a1ed5dfbe9e9ff7bc24609d392fb2b55e8cd
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-02 19:31:10 +0200

    test: Test RaftLog properties

---
+++ src/tests/entities/test_raft_log.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+from entities.leader_state import LeaderState
+from entities.raft_log import RaftLog
+from entities.sat_formula import SatFormula
+
+
+def test_get_completed_tasks():
+    leader_state = LeaderState(completed_tasks=[1, 2, 6, 10])
+
+    log = RaftLog(leader_state=leader_state)
+    log.leader_state.completed_tasks = [1, 2, 3]
+
+    assert log.completed_tasks == [1, 2, 3]
+
+
+def test_get_current_formula():
+    formula1 = [(1, 2, 3), (-1, -2, 3)]
+    formula2 = [(4, -5, 7), (-4, 5, 6)]
+    formula3 = [(4, -5, 7), (-4, 5, 6), (1, -3, 2)]
+
+    formulas: deque[SatFormula] = deque(
+        [
+            formula1,
+            formula2,
+            formula3,
+        ]
+    )
+
+    leader_state = LeaderState(formulas=formulas)
+    log = RaftLog(leader_state=leader_state)
+
+    assert log.current_formula == formula1
