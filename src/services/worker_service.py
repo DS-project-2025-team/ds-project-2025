4a3466c15a10c71d2d313efdb4c0284665dfcffc
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-06 20:06:13 +0200

    feat: Add WorkerService.run_task()

--- src/services/worker_service.py
+++ src/services/worker_service.py
@@ -1,7 +1,13 @@
+import asyncio
+from collections.abc import Callable, Coroutine
 from contextlib import AbstractAsyncContextManager
 from multiprocessing.pool import Pool
 from types import TracebackType
 from typing import Self
+
+from entities.sat_formula import SatFormula
+from utils.check_sat import check_sat_formula
+from utils.task import get_subinterval
 
 
 class WorkerService(AbstractAsyncContextManager):
@@ -19,3 +25,20 @@ class WorkerService(AbstractAsyncContextManager):
         traceback: TracebackType | None,
     ) -> None:
         self.__pool.__exit__(exc_type, exc_value, traceback)
+
+    def run_task(
+        self,
+        formula: SatFormula,
+        task: int,
+        exponent: int,
+        callback: Callable[[bool], Coroutine],
+    ) -> None:
+        self.__pool.apply_async(
+            self.__compute,
+            (formula, task, exponent),
+            callback=lambda result: asyncio.run(callback(result)),
+        )
+
+    def __compute(self, formula: SatFormula, task: int, exponent: int) -> bool:
+        begin, end = get_subinterval(2**exponent, task)
+        return check_sat_formula(formula, begin, end)
