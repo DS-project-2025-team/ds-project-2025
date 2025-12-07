bf05e3be238ed53be55d944ff6000ca35e5dc116
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-07 17:15:20 +0200

    feat: Add PingService.__init__()

--- src/services/ping_service.py
+++ src/services/ping_service.py
@@ -1,4 +1,10 @@
 from contextlib import AbstractAsyncContextManager
+from uuid import UUID
+
+from entities.server_address import ServerAddress
+from network.message_consumer import MessageConsumer
+from network.message_consumer_factory import MessageConsumerFactory
+from network.message_producer import MessageProducer
 
 
 class PingService(AbstractAsyncContextManager):
@@ -6,5 +12,14 @@ class PingService(AbstractAsyncContextManager):
     Class for counting alive consumers, assuming one consumer per group.
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        server: ServerAddress,
+        node_id: UUID,
+        count: int = 0,
+    ) -> None:
+        self.__count: int = count
+        self.__producer: MessageProducer = MessageProducer(server)
+        self.__consumer: MessageConsumer = (
+            MessageConsumerFactory.ping_response_consumer(server, node_id)
+        )
