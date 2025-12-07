4d4def1fd399a8a263e92a7dbbed64e79bec8c07
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-07 20:15:57 +0200

    refactor: Move Follower into raft.roles

--- src/raft/node.py
+++ src/raft/node.py
@@ -10,7 +10,7 @@ from network.message_consumer import MessageConsumer
 from network.message_consumer_factory import MessageConsumerFactory
 from network.message_producer import MessageProducer
 from network.topic import Topic
-from raft.follower import Follower
+from raft.roles.follower import Follower
 from raft.roles.leader import Leader
 from raft.role import Role
 from services.logger_service import logger
