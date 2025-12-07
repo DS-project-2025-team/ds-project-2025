5e9d9d815895c41f6a2bcd8f9efaf8cb2f5be7ce
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-07 20:41:39 +0200

    refactor(candidate): Use term from log

--- src/raft/roles/candidate.py
+++ src/raft/roles/candidate.py
@@ -59,41 +59,41 @@ class Candidate(AbstractAsyncContextManager):
         await self.__appendentry_consumer.__aexit__(exc_type, exc_value, traceback)
 
     async def run(self) -> Role:
-        term = self.__log.term + 1
+        self.__log.term += 1
         nodes = await self.__count_nodes()
 
         role = Role.FOLLOWER
 
         try:
-            await asyncio.create_task(self.__check_leader_existence(term))
+            await asyncio.create_task(self.__check_leader_existence())
 
             role = await asyncio.wait_for(
-                self.__elect(term, nodes), timeout=self.__vote_timeout
+                self.__elect(nodes), timeout=self.__vote_timeout
             )
 
         except (LeaderExistsError, OutDatedTermError) as error:
             logger.info(str(error))
 
         except TimeoutError:
-            logger.info(f"Election for term {term} timed out.")
+            logger.info(f"Election for term {self.__log.term} timed out.")
             role = Role.CANDIDATE
 
         return role
 
-    async def __elect(self, term: int, nodes: int) -> Role:
-        logger.info(f"Starting election for term {term}")
+    async def __elect(self, nodes: int) -> Role:
+        logger.info(f"Starting election for term {self.__log.term}")
 
-        await self.__send_vote_request(term)
-        await self.__wait_for_votes(term, nodes)
+        await self.__send_vote_request()
+        await self.__wait_for_votes(nodes)
 
         return Role.LEADER
 
-    async def __send_vote_request(self, term: int) -> None:
+    async def __send_vote_request(self) -> None:
         last_log_index = self.__log.last_log_index
         last_log_term = self.__log.last_log_term
 
         payload = {
-            "term": term,
+            "term": self.__log.term,
             "candidate_id": self.__id,
             "last_log_index": last_log_index,
             "last_log_term": last_log_term,
@@ -102,25 +102,25 @@ class Candidate(AbstractAsyncContextManager):
         await self.__producer.send(Topic.VOTE_REQUEST, payload)
         logger.info("Sent vote requests")
 
-    async def __wait_for_votes(self, term: int, nodes: int) -> None:
+    async def __wait_for_votes(self, nodes: int) -> None:
         votes = 0
         votes_required = nodes // 2 + 1
 
         while votes < votes_required:
-            votes += await self.__receive_vote(term)
+            votes += await self.__receive_vote()
 
     async def __count_nodes(self) -> int:
         return await self.__ping_service.count_consumers()
 
-    async def __receive_vote(self, term: int) -> int:
+    async def __receive_vote(self) -> int:
         message = await self.__vote_consumer.receive()
 
         vote_term = message.data["term"]
         vote_granted = message.data["vote_granted"]
 
-        if vote_term > term:
+        if vote_term > self.__log.term:
             self.__log.term = vote_term
-            raise OutDatedTermError(term, vote_term)
+            raise OutDatedTermError(self.__log.term, vote_term)
 
         if not vote_granted:
             return 0
@@ -128,10 +128,10 @@ class Candidate(AbstractAsyncContextManager):
         return 1
 
     @async_loop
-    async def __check_leader_existence(self, term: int) -> None:
+    async def __check_leader_existence(self) -> None:
         message = await self.__appendentry_consumer.receive()
 
         leader_term = message.data["term"]
 
-        if leader_term >= term:
+        if leader_term >= self.__log.term:
             raise LeaderExistsError()
