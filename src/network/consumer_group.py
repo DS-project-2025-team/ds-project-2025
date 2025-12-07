8454142bd1230b17d40f29674980cdb06f2eff51
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-07 17:02:11 +0200

    feat: Add factory methods to ConsumerGroup

--- src/network/consumer_group.py
+++ src/network/consumer_group.py
@@ -1,2 +1,22 @@
+from typing import Self
+from uuid import UUID
+
+from roles.role import Role
+
+
 class ConsumerGroup(str):
-    pass
+    @classmethod
+    def leader(cls) -> Self:
+        return cls(Role.LEADER)
+
+    @classmethod
+    def canditate(cls) -> Self:
+        return cls(Role.CANDIDATE)
+
+    @classmethod
+    def follower(cls) -> Self:
+        return cls(Role.FOLLOWER)
+
+    @classmethod
+    def node(cls, node_id: UUID) -> Self:
+        return cls(node_id)
