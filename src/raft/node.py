8b5c4b214ecd61ca68fcd8d381e041077f66d2d0
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-07 20:15:45 +0200

    refactor: Move Leader into raft.roles

--- src/raft/node.py
+++ src/raft/node.py
@@ -11,7 +11,7 @@ from network.message_consumer_factory import MessageConsumerFactory
 from network.message_producer import MessageProducer
 from network.topic import Topic
 from raft.follower import Follower
-from raft.leader import Leader
+from raft.roles.leader import Leader
 from raft.role import Role
 from services.logger_service import logger
 from utils.async_loop import async_loop
