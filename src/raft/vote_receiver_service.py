6ee841a45eee18c4d770511ea54ff7bb48a2226f
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-08 20:17:17 +0200

    refactor: Pass log into VoteReceiverService

--- src/raft/vote_receiver_service.py
+++ src/raft/vote_receiver_service.py
@@ -6,13 +6,16 @@ from entities.server_address import ServerAddress
 from network.message_consumer import MessageConsumer
 from network.message_consumer_factory import MessageConsumerFactory
 from network.message_producer import MessageProducer
+from raft.entities.log import Log
 from utils.async_loop import async_loop
 
 
 class VoteReceiverService(AbstractAsyncContextManager):
     def __init__(
-        self, server: ServerAddress, node_id: UUID, producer: MessageProducer
+        self, server: ServerAddress, node_id: UUID, producer: MessageProducer, log: Log
     ) -> None:
+        self.__log: Log = log
+
         self.__producer: MessageProducer = producer
         self.__vote_consumer: MessageConsumer = MessageConsumerFactory.vote_consumer(
             server, node_id
