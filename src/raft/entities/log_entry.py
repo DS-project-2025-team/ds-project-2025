fc36a01ab436bb1c58dec95426ebf516bcda6e13
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-11 16:14:01 +0200

    refactor: Simplify LogEntry

--- src/raft/entities/log_entry.py
+++ src/raft/entities/log_entry.py
@@ -4,14 +4,13 @@ from raft.entities.leader_state import LeaderState
 
 
 class LogEntry:
-    def __init__(
-        self, raftlog: "RaftLog", operation: Callable[[LeaderState], None]
-    ) -> None:
-        term = raftlog.term
-        self.__raftlog = raftlog
+    def __init__(self, leader_state: LeaderState, term: int, index: int) -> None:
+        self.__leader_state = leader_state
         self.__term = term
-        self.__index = 0
-        self.__operation: Callable[[LeaderState], None] = operation
+        self.__index = index
+        self.__operation: Callable[[LeaderState], LeaderState] = (
+            lambda _: self.__leader_state
+        )
 
     def operate(self, state: LeaderState) -> None:
         self.__operation(state)
@@ -29,14 +28,8 @@ class LogEntry:
         self.__index = index
 
     def to_dict(self) -> dict[str, object]:
-        raftlog = self.__raftlog
-        prev_index = self.__index - 1
-        prev_log_entry = raftlog.get_entry_at_index(prev_index)
-        commit_index = raftlog.get_commit_index()
         return {
             "term": self.__term,
             "index": self.__index,
-            "prev_log_index": prev_index,
-            "prev_log_term": prev_log_entry.__term,
-            "commit_index": commit_index,
+            "state": self.__leader_state.__dict__,
         }
