607b3b911a97322c18a752cbaf1fd8f7045a6c87
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2025-12-12 17:19:45 +0200

    refactor: Use PartialLogEntry in LogEntryFactory

--- src/raft/entities/log_entry_factory.py
+++ src/raft/entities/log_entry_factory.py
@@ -2,41 +2,41 @@ from operator import concat
 
 from entities.sat_formula import SatFormula
 from raft.entities.leader_state import LeaderState
-from raft.entities.log_entry import LogEntry
+from raft.entities.partial_log_entry import PartialLogEntry
 
 
 class LogEntryFactory:
     @staticmethod
-    def complete_task(task: int, state: LeaderState, term: int, index: int) -> LogEntry:
+    def complete_task(task: int, state: LeaderState, term: int) -> PartialLogEntry:
         formulas = state.formulas.copy()
         completed_tasks = (
             (i == task) or x for i, x in enumerate(state.completed_tasks)
         )
 
-        return LogEntry(LeaderState(completed_tasks, formulas), term, index)
+        return PartialLogEntry(LeaderState(completed_tasks, formulas), term)
 
     @staticmethod
     def add_formula(
-        formula: SatFormula, state: LeaderState, term: int, index: int
-    ) -> LogEntry:
+        formula: SatFormula, state: LeaderState, term: int
+    ) -> PartialLogEntry:
         formulas = concat(list(state.formulas), [formula])
         completed_tasks = state.completed_tasks.copy()
 
-        return LogEntry(LeaderState(completed_tasks, formulas), term, index)
+        return PartialLogEntry(LeaderState(completed_tasks, formulas), term)
 
     @staticmethod
-    def pop_formula(state: LeaderState, term: int, index: int) -> LogEntry:
+    def pop_formula(state: LeaderState, term: int) -> PartialLogEntry:
         formulas = []
 
         if state.formulas:
             formulas = list(state.formulas)[1:]
 
-        return LogEntry(LeaderState([], formulas), term, index)
+        return PartialLogEntry(LeaderState([], formulas), term)
 
     @staticmethod
     def set_completed_tasks(
-        completed_tasks: list[bool], state: LeaderState, term: int, index: int
-    ) -> LogEntry:
+        completed_tasks: list[bool], state: LeaderState, term: int
+    ) -> PartialLogEntry:
         formulas = state.formulas.copy()
 
-        return LogEntry(LeaderState(completed_tasks, formulas), term, index)
+        return PartialLogEntry(LeaderState(completed_tasks, formulas), term)
